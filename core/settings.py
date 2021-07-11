51ac8a98bca889b6cec18c9c27aad2e4afd60379
Anuj Mehere <77264766+anujmehere@users.noreply.github.com>
2021-07-11 11:11:18 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -145,4 +145,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "iamanujnmehere@gmail.com"
-EMAIL_HOST_PASSWORD = "Mac@1010"
+EMAIL_HOST_PASSWORD = "Hitler@1889"
